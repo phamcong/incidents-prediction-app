7f4557d50096d1924bd0159ef62df7d53df0a930
phamcong <ccuong.ph@gmail.com>
2018-10-23 00:31:56 +0200

    update result image base64 path

--- incidents-prediction-app/src/app/components/models/models.component.ts
+++ incidents-prediction-app/src/app/components/models/models.component.ts
@@ -3,6 +3,7 @@ import * as demoData from '../../shared/demo-data';
 import { Model } from 'src/app/shared/models/model';
 import { ModelsService } from 'src/app/services/models.service';
 import { Observable } from 'rxjs';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-models',
@@ -13,10 +14,12 @@ import { Observable } from 'rxjs';
 
 export class ModelsComponent implements OnInit {
   public models$: Observable<any>;
+  public calledModel$: Observable<any>;
   public models: Model[];
   public selectedModel: Model;
+  public resultImageBase64Path: string;
 
-  constructor( private ms: ModelsService ) { }
+  constructor( private ms: ModelsService, private _sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.models$ = this.ms.getModels()
@@ -29,5 +32,21 @@ export class ModelsComponent implements OnInit {
   changeModel($event) {
     this.selectedModel = $event.target.value;
     console.log('selected model: ', this.selectedModel);
+  }
+
+  callPredictionModel() {
+    const selectedParameters = {};
+    this.selectedModel.parameters.map(parameter => {
+      if (parameter['selectedValue'] !== undefined) {
+        const selectedValue = parameter['selectedValue'];
+        selectedParameters[parameter['label']] = selectedValue['type_value'] === 'number' ? +selectedValue['value'] : selectedValue['value']
+      }
+    });
+
+    this.calledModel$ = this.ms.callPredictionModel(this.selectedModel['id'], selectedParameters)
+    .subscribe(data => {
+      this.resultImageBase64Path = this._sanitizer.bypassSecurityTrustResourceUrl('data:image/jpg;base64,' + data['result']);
+      console.log('resultImageBase64Path: ', this.resultImageBase64Path);
+    });
   }
 }
--- incidents-prediction-app/src/app/services/models.service.ts
+++ incidents-prediction-app/src/app/services/models.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
-export class ModelsService {
 
+export class ModelsService {
   constructor( private http: HttpClient ) { }
 
   getModels(): any {
     return this.http.get('http://localhost:8080/api/predictionmodels')
     .pipe(map(res => res ));
   }
+
+  callPredictionModel(modelID, parameters): any {
+    return this.http.post('http://localhost:8080/api/predictionmodels/' + modelID + '/', {'parameters': parameters}, httpOptions)
+    .pipe(map(res => res));
+  }
 }
+
